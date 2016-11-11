7ead322de6a09cb7a6b0c336124defd5d5e13fd7
meghanroserebecca <meghanroserebecca@gmail.com>
2016-11-11 13:55:01 -0800

    added multiply function and passed test

--- app.js
+++ app.js
@@ -31,11 +31,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var productNumber = a * b;
+  var message = ('The product of ' + a + ' and ' + b + ' is ' + productNumber + '.');
+  console.log(message + ' ' + productNumber + '.');
+  return [productNumber, message];
 }
 
+multiply(5,9);
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
